6c1cf63d62e1d17c33d168a584920a988b9aa5ed
arsanyattalla <arsanyattalla26@gmail.com>
2024-12-26 17:30:43 -0800

    fixes

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -65,10 +65,10 @@ function App() {
       }
 
       try {
-        //const response = await fetch(`/.netlify/functions/api?city=${city}`);
-        const response = await fetch(
-          `http://localhost:5000/weather?city=${city}`
-        );
+        const response = await fetch(`/.netlify/functions/api?city=${city}`);
+        //const response = await fetch(
+          //`http://localhost:5000/weather?city=${city}`
+        //);
 
         const data = await response.json();
         console.log(data);
@@ -374,7 +374,7 @@ function App() {
         >
           <div className="content-wrapper">
             <div className="header-image">
-          <img className="img"  src={'./weather-icon-illustration03-Graphics-10205167-1.jpg'}></img>
+          <img className="img" alt="img"  src={'./weather-icon-illustration03-Graphics-10205167-1.jpg'}></img>
             <h1 className="title">Weather</h1>
             </div>
             <div className="header">
