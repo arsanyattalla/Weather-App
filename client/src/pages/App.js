bad5a189a7e9473e5d90b736c9f774e00cd0c67d
arsanyattalla <arsanyattalla26@gmail.com>
2025-01-04 15:08:10 -0800

    fixed-api-call

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -62,10 +62,10 @@ function App() {
       }
 
       try {
-        //const response = await fetch(`/.netlify/functions/api?city=${city}`);
-        const response = await fetch(
-          `http://localhost:5000/weather?city=${city}`
-        );
+        const response = await fetch(`/.netlify/functions/api?city=${city}`);
+        //const response = await fetch(
+          //`http://localhost:5000/weather?city=${city}`
+        //);
 
         const data = await response.json();
         console.log(data);
