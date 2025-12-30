8f961d72ccf2e42575e05b2776940f39eeb4545e
arsanyattalla <arsanyattalla26@gmail.com>
2025-12-29 23:07:24 -0800

    fix

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import "../css/App.css";
 import image from "../images/clearsky.png";
 import cloud from "../images/cloudy.png";
